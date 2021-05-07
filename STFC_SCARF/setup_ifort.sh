7233c41617541b7f96b38545b9f18a71697c0724
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2021-05-07 14:33:12 +0100

    <bugfix>[STFC_SCARF] setup_ifort loading of intel fortran compiler changed in attempt to fix installation on STFC SCARF

--- STFC_SCARF/setup_ifort.sh
+++ STFC_SCARF/setup_ifort.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo 'Load Intel Fortran Libraries for PTC compilation'
-module load intel/20.0.0
+#module load intel/20.0.0
+module load iomkl/2019b #Easybuild toolchain for intel compiler, openmpi, and MKL as suggested by Derek Ross on 07.05.21
 which ifort
 which iccvars
