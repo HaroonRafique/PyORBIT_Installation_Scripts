6c7dd8084d911d60868bdb9b9d3a41c3c8e1316c
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2022-01-31 14:50:28 +0000

    <update>[SCARF] merged PTC

--- STFC_SCARF/Working_Jan_2022/01_PTC_Merge_PyORBIT/02_PyORBIT_install.sh
+++ STFC_SCARF/Working_Jan_2022/01_PTC_Merge_PyORBIT/02_PyORBIT_install.sh
@@ -4,6 +4,7 @@
 
 ORBIT_ROOT="${PWD}"
 PYORBIT_DIR="${ORBIT_ROOT}/py-orbit"
+export FFTW3_ROOT="${ORBIT_ROOT}/include"
 
 [[ -d ${PYORBIT_DIR} ]] && rm -rf ${PYORBIT_DIR}
 
@@ -24,12 +25,19 @@ cd ${ORBIT_ROOT}
 sed -i 's/.*#DIRS += .\/PTC*/DIRS += .\/PTC/' "${PYORBIT_DIR}/ext/Makefile"
 sed -i 's/.*DIRS += .\/ecloud*/#DIRS += .\/ecloud/' "${PYORBIT_DIR}/ext/Makefile"
 
+# For NewPTC version we should switch the COMP flag to GNU when not using iFort
+#sed -i 's/COMP=intel/COMP=gnu/' $pyorbit_dir/Makefile
+
 # load required modules
 ml ifort/2018.3.222-GCC-7.3.0-2.30
+#source /apps/eb/software/ifort/2018.3.222-GCC-7.3.0-2.30/compilers_and_libraries_2018.3.222/linux/bin/ifortvars.sh intel64
+source /apps/eb/software/ifort/2018.3.222-GCC-7.3.0-2.30/compilers_and_libraries_2018.3.222/linux/bin/compilervars.sh intel64
+export PROD_DIR INTEL_TARGET_ARCH
 
 # activate environment
 source "${ORBIT_ROOT}/virtualenvs/py2.7/bin/activate"
 source "${PYORBIT_DIR}/customEnvironment.sh"
+#source "${PYORBIT_DIR}/setupEnvironment.sh"
 
 # build pyorbit
 echo
@@ -37,4 +45,6 @@ echo "Building pyORBIT..."
 echo "-------------------"
 echo
 cd ${PYORBIT_DIR}
-make -j ${SLURM_NPROCS} | tee -a pyorbit_install.log
+#make -j ${SLURM_NPROCS} | tee -a pyorbit_install.log
+# For some reason this compilation prefers a single core installation
+make | tee -a pyorbit_install.log
